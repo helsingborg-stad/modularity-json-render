07fb6e7bd2bbb3252ec5a525ff39ddce9d48330d
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-09-28 10:00:11 +0200

    Cleaned up master file (Moved to app)

--- source/php/App.php
+++ source/php/App.php
@@ -6,8 +6,68 @@ class App
 {
     public function __construct()
     {
+        //Enqueue scripts
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+
+        //Init ACF import
+        add_action('plugins_loaded', array($this, 'initAcfImportExport'));
+
+        //Init module
+        add_action('plugins_loaded', array($this, 'registerModule'));
+        add_action('Modularity/Module/TemplatePath', array($this, 'registerModuleTemplate'));
+
+        //Run subclasses
+        add_action('init', array($this, 'init'));
+    }
+
+    /**
+     * Init required subclasses
+     * @return void
+     */
+    public function init()
+    {
+        new \ModularityJsonRender\Module\JsonRender();
+    }
+
+    /**
+     * Register the module
+     * @return void
+     */
+    public function registerModule()
+    {
+        if (function_exists('modularity_register_module')) {
+            modularity_register_module(
+                MODULARITYJSONRENDER_PATH . 'Source/php/Module/',
+                __('Json Render', 'modularity-json-render')
+            );
+        }
+    }
+
+    /**
+     * Register module view
+     * @return array
+     */
+
+    public function registerModuleTemplate($paths)
+    {
+        $paths[] = MODULARITYJSONRENDER_PATH . 'Source/php/Module/views/';
+        return $paths;
+    }
+
+    /**
+     * Run ACF import & export funcitonality
+     * @return void
+     */
+    public function initAcfImportExport()
+    {
+        $acfExportManager = new \AcfExportManager\AcfExportManager();
+        $acfExportManager->setTextdomain('modularity-json-render');
+        $acfExportManager->setExportFolder(MODULARITYJSONRENDER_PATH . 'acf-fields/');
+        $acfExportManager->autoExport(array(
+            // 'base' => 'group_599eaa60c0e79',
+        ));
+        $acfExportManager->import();
     }
 
     /**
