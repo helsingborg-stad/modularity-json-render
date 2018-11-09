582ac43ac7caeb8e807a667e969e8293bc5a4865
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-11-09 16:41:03 +0100

    Styled json tree

--- source/js/Admin/Components/ListItem.js
+++ source/js/Admin/Components/ListItem.js
@@ -1,20 +1,32 @@
 function ListItem(props) {
     const {value, children, fieldMap, object, onClickTitle, onClickContent, onClickContainer} = props;
-    return (<li>
-        {fieldMap.title === object && fieldMap.title  ? <strong>Title: </strong> : ''}
-        {fieldMap.content === object && fieldMap.content ? <strong>Content: </strong> : ''}
-        {children ? <strong>{value}</strong> : <span>{value}</span>}
-        {!children && !fieldMap.title && (fieldMap.content !== object) && fieldMap.itemContainer !== null ?
-            <a href="#" className="button button-small" data-field="title" onClick={onClickTitle}>Title</a> : ''}
-        {!children && (fieldMap.title !== object) && !fieldMap.content && fieldMap.itemContainer !== null ?
-            <a href="#" className="button button-small" data-field="content"
-               onClick={onClickContent}>Content</a> : ''}
-        {children && Array.isArray(object) && fieldMap.itemContainer === null ?
-            <a href="#" className="button button-small" data-field="itemContainer"
-               onClick={onClickContainer}>Select</a> : ''}
-        {children ? <span className="dashicons dashicons-arrow-down"></span> : ''}
-        {children ? <ul className="sub-object">{children}</ul> : ''}
-    </li>);
+
+    if (children) {
+        return (<li>
+            { Array.isArray(object) ? <span><span className="dashicons dashicons-portfolio"></span> <strong>{value}</strong></span> : <span>{value}</span>}
+
+            <span className="dashicons dashicons-arrow-down"></span>
+
+            {Array.isArray(object) && fieldMap.itemContainer === null ?
+                <a href="#" className="button button-primary button-small field-select" data-field="itemContainer"
+                   onClick={onClickContainer}>Select</a> : ''}
+
+            <ul className="sub-object">{children}</ul>
+        </li>);
+    } else {
+        return (<li>
+            {fieldMap.title === object && fieldMap.title ? <strong>Title: </strong> : ''}
+            {fieldMap.content === object && fieldMap.content ? <strong>Content: </strong> : ''}
+
+            <span>{value}</span>
+
+            {!fieldMap.title && (fieldMap.content !== object) && fieldMap.itemContainer !== null ?
+                <a href="#" className="button button-small" data-field="title" onClick={onClickTitle}>Title</a> : ''}
+            {!fieldMap.content && (fieldMap.title !== object) && fieldMap.itemContainer !== null ?
+                <a href="#" className="button button-small" data-field="content"
+                   onClick={onClickContent}>Content</a> : ''}
+        </li>);
+    }
 }
 
 export default ListItem;
