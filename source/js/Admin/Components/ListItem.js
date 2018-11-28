ef382f15336b1a308f92a6518c0d2387b8055720
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-11-28 16:58:15 +0100

    simplified file

--- source/js/Admin/Components/ListItem.js
+++ source/js/Admin/Components/ListItem.js
@@ -1,35 +1,48 @@
 import DragItem from './DragItem';
 import uuidv1 from "uuid/v1";
 
-const ListItem = ({field, value, sample, children, fieldMap, onClickTitle, onClickContainer, translation}) => {
-    if (children) {
-        return (<li>
-            {Array.isArray(value) && fieldMap.itemContainer === null ?
-                <span><strong>
-                    <span className="dashicons dashicons-portfolio"></span> {field}
-                    <a href="#"
-                       className="tree-select"
-                       data-field="itemContainer"
-                       onClick={onClickContainer}>{translation.select}</a>
-                    </strong></span> :
-                <span>{field}</span>}
-            <ul>{children}</ul>
-        </li>);
-    } else {
-        const item = {
-            field: field,
-            value: value,
-            sample: sample
-        };
+const ListItem = ({field, value, sample, children, fieldMap, onClickContainer, translation}) => {
 
-        return (
-            <li>{fieldMap.itemContainer === null ? <span>{field}</span> :
-                <DragItem
-                    id={uuidv1()}
-                    item={item}
-                />
-            }</li>
-        );
+    if (fieldMap.itemContainer === null) {
+
+        if (children) {
+            return (<li>
+                {Array.isArray(value) ?
+                    <span><strong>
+                    <span className="dashicons dashicons-portfolio"></span> {field}
+                        <a href="#"
+                           className="tree-select"
+                           data-field="itemContainer"
+                           onClick={onClickContainer}>{translation.select}</a>
+                    </strong></span> :
+                    <span>{field}</span>}
+
+                <ul>{children}</ul>
+            </li>);
+        } else {
+            return <li><span>{field}</span></li>;
+        }
+
+    } else {
+        if (children) {
+            return <ul>{children}</ul>;
+        } else {
+            const item = {
+                field: field,
+                value: value,
+                sample: sample
+            };
+            return (
+                <li>
+                    <DragItem
+                        id={uuidv1()}
+                        heading={field}
+                        item={item}
+                    />
+                </li>
+            );
+        }
+
     }
 };
 
