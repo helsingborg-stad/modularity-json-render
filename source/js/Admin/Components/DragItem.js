fe555cc0abdd5030aa5fa24f27c7394f77313e4f
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-12-04 17:34:05 +0100

    Use WP sortable menus style on draggable items

--- source/js/Admin/Components/DragItem.js
+++ source/js/Admin/Components/DragItem.js
@@ -1,21 +1,42 @@
+import {translation} from '../Config/config';
 import {DragSource, DropTarget} from 'react-dnd';
 
 class DragItem extends React.Component {
     render() {
         const {listId, item, heading, headingChange, removeItem, index} = this.props;
         const {isDragging, connectDragSource, connectDropTarget} = this.props;
-        const opacity = isDragging ? 'transparent' : '';
+        const opacity = isDragging ? 'drop-area__item--transparent' : '';
 
         let sample = typeof item.sample === 'string' || typeof item.sample === 'number' ? item.sample : '';
         sample = (sample.length > 50) ? sample.substring(0, 50) + '...' : sample;
 
         return connectDragSource(connectDropTarget(
-            <div className={`drag-item drag-item--${opacity}`}>
-                <p>
-                    <strong>{item.field}:</strong> <i>{sample}</i>
-                    {listId && <a href="#" className="remove-item" onClick={(e) => {e.preventDefault(); removeItem(index);}}><span className="dashicons dashicons-no"></span></a>}
-                </p>
-                {listId && <p><strong>Title:</strong><input type="text" name="" onChange={headingChange} value={heading} className=".regular-text"/></p>}
+            <div className={`drop-area__item ${opacity}`}>
+                <div className="menu-item-bar">
+                    <div className="menu-item-handle">
+                        <span className="item-title">
+                            <span className="menu-item-title">{item.field}</span>
+                        </span>
+                        <span className="item-controls">
+                            {listId &&
+                            <a href="#" className="item-edit" onClick={(e) => {e.preventDefault();removeItem(index);}}></a>
+                            }
+                        </span>
+                    </div>
+                </div>
+                <div className="menu-item-settings wp-clearfix">
+                    <p className="description description-wide">
+                        {translation.value}: {sample}
+                    </p>
+                    {listId &&
+                    <p className="description description-wide">
+                        <label>
+                            {translation.title}<br/>
+                            <input type="text" name="" onChange={headingChange} value={heading} className="large-text"/>
+                        </label>
+                    </p>
+                    }
+                </div>
             </div>
         ));
     }
