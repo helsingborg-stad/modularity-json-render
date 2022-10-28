743e698cb0443d42ddef266eb516f3c762614001
NiclasNorin <niclas.norin@frontwalker.se>
2022-10-28 16:18:56 +0200

    styling
    Centering and making it look like the other components

--- source/js/Front/Components/SearchField.js
+++ source/js/Front/Components/SearchField.js
@@ -1,9 +1,12 @@
 const SearchField = ({doSearch, translation}) =>
     <div class="c-card__body">
         <div class="c-field c-field__text">
-            <input aria-labelledby="c-field__03795285" type="text" name="json-render-search" onChange={doSearch} placeholder={translation.filterOn} />
             <label id="c-field__03795285" class="c-field__text--label">{translation.search}</label>
+            <div class="c-field__inner c-field__inner--text">
+            <input aria-labelledby="c-field__03795285" type="text" name="json-render-search" onChange={doSearch} placeholder={translation.filterOn} />
+            </div>
         </div>
     </div>;
+    
 
 export default SearchField;
