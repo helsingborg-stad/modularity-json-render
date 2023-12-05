6517b9f56c104e44b3b2af7e3984ec1e32630e2e
GitHub Action <>
2023-12-05 09:36:24 +0000

    build: update package file versions to 3.0.3

--- modularity-json-render.php
+++ modularity-json-render.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Json Render
  * Plugin URI:        https://github.com/helsingborg-stad/modularity-json-render
  * Description:       Renders JSON api:s as a list etc.
- * Version: 3.0.2
+ * Version: 3.0.3
  * Author:            Sebastian Thulin, Jonatan Hanson
  * Author URI:        https://github.com/sebastianthulin
  * License:           MIT
