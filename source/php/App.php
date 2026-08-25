7032fbbf73f3bc0f47b04056d2dac1755498bc9a
NiclasNorin <niclasn@outlook.com>
2026-08-25 08:46:43 +0200

    fix: remove old asset enqueue

--- source/php/App.php
+++ source/php/App.php
@@ -39,15 +39,6 @@ class App
     }
 
     /**
-     * Register required admin scripts & styles
-     * @return void
-     */
-    public function registerAdminAssets()
-    {
-        $this->wpEnqueue->add('css/modularity-json-render-admin.css')->add('js/Admin/IndexAdmin.js', ['jquery', 'react', 'react-dom'], false, true);
-    }
-
-    /**
      * Register meta boxes
      * @return void
      */
